1a4318968bfaa55a8a70b7a13758f2bba2854f14
unknown <phuclh1406@gmail.com>
2023-09-27 23:12:20 +0700

    hind home app bar when scrolling down

--- lib/screens/driver/home_screen.dart
+++ lib/screens/driver/home_screen.dart
@@ -58,195 +58,204 @@ class _DriverHomeScreenState extends State<DriverHomeScreen> {
     final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 250, 250, 250),
-      appBar: AppBar(
-        title: const Center(
-          child: Text(
-            'Welcome to NBTour',
-            style: TextStyle(fontSize: 16),
-          ),
-        ),
-        iconTheme: const IconThemeData(color: Colors.white),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.notifications,
-              color: Colors.white,
-            ),
-          )
-        ],
-      ),
       drawer: MainDrawer(
         userName: userName,
         avatar: avatar,
         onSelectScreen: (ex) {},
       ),
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Container(
-                  width: screenWidth - kItemPadding * 2,
-                  decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 255, 255, 255),
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(kDefaultPadding),
-                        bottomRight: Radius.circular(kDefaultPadding)),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool isScrolled) {
+          return [
+            SliverAppBar(
+              title: const Center(
+                child: Text(
+                  'Welcome to NBTour',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              iconTheme: const IconThemeData(color: Colors.white),
+              actions: <Widget>[
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.notifications,
+                    color: Colors.white,
                   ),
-                  padding: const EdgeInsets.all(kDefaultPadding),
+                )
+              ],
+            )
+          ];
+        },
+        floatHeaderSlivers: true,
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    width: screenWidth - kItemPadding * 2,
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(kDefaultPadding),
+                          bottomRight: Radius.circular(kDefaultPadding)),
+                    ),
+                    padding: const EdgeInsets.all(kDefaultPadding),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ActionCategoryWidget(
+                            icon: const Icon(
+                              Icons.shopping_cart_checkout_outlined,
+                              size: kDefaultIconSize * 1.2,
+                              color: Colors.white,
+                            ),
+                            onTap: () {},
+                            title: 'Sell Products'),
+                        const SizedBox(width: kItemPadding / 1.5),
+                        ActionCategoryWidget(
+                            icon: const Icon(
+                              Icons.calendar_month_outlined,
+                              size: kDefaultIconSize * 1.2,
+                              color: Colors.white,
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        const DriverScheduleScreen(),
+                                  ));
+                            },
+                            title: 'Your Schedule'),
+                        const SizedBox(width: kItemPadding / 1.5),
+                        ActionCategoryWidget(
+                            icon: const Icon(
+                              Icons.work_history_outlined,
+                              size: kDefaultIconSize * 1.2,
+                              color: Colors.white,
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (ctx) =>
+                                          const DriverTourScreen()));
+                            },
+                            title: 'Your Activity'),
+                        const SizedBox(width: kItemPadding / 1.5),
+                        ActionCategoryWidget(
+                            icon: const Icon(
+                              Icons.history_outlined,
+                              size: kDefaultIconSize * 1.2,
+                              color: Colors.white,
+                            ),
+                            onTap: () {},
+                            title: 'Tour History'),
+                        const SizedBox(width: kItemPadding / 1.5),
+                        ActionCategoryWidget(
+                            icon: const Icon(
+                              Icons.qr_code_scanner,
+                              size: kDefaultIconSize * 1.2,
+                              color: Colors.white,
+                            ),
+                            onTap: () {},
+                            title: 'QR Scanner'),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: kMediumPadding / 2),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  child: Text('Announcement',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20)),
+                ),
+                const SizedBox(height: kMediumPadding / 2),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      ActionCategoryWidget(
-                          icon: const Icon(
-                            Icons.shopping_cart_checkout_outlined,
-                            size: kDefaultIconSize * 1.2,
-                            color: Colors.white,
-                          ),
-                          onTap: () {},
-                          title: 'Sell Products'),
-                      const SizedBox(width: kItemPadding / 1.5),
-                      ActionCategoryWidget(
-                          icon: const Icon(
-                            Icons.calendar_month_outlined,
-                            size: kDefaultIconSize * 1.2,
-                            color: Colors.white,
-                          ),
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (ctx) =>
-                                        const DriverScheduleScreen()));
-                          },
-                          title: 'Your Schedule'),
-                      const SizedBox(width: kItemPadding / 1.5),
-                      ActionCategoryWidget(
-                          icon: const Icon(
-                            Icons.work_history_outlined,
-                            size: kDefaultIconSize * 1.2,
-                            color: Colors.white,
-                          ),
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (ctx) =>
-                                        const DriverTourScreen()));
-                          },
-                          title: 'Your Activity'),
-                      const SizedBox(width: kItemPadding / 1.5),
-                      ActionCategoryWidget(
-                          icon: const Icon(
-                            Icons.history_outlined,
-                            size: kDefaultIconSize * 1.2,
-                            color: Colors.white,
-                          ),
-                          onTap: () {},
-                          title: 'Tour History'),
-                      const SizedBox(width: kItemPadding / 1.5),
-                      ActionCategoryWidget(
-                          icon: const Icon(
-                            Icons.qr_code_scanner,
-                            size: kDefaultIconSize * 1.2,
-                            color: Colors.white,
-                          ),
-                          onTap: () {},
-                          title: 'QR Scanner'),
+                      AnnouncementWidget(
+                        onTap: () {},
+                        announcementImage: ImageHelper.loadFromAsset(
+                            AssetHelper.announcementImage),
+                        title: 'International Band Museum312312321312',
+                        author: 'Nhan Nguyen',
+                        dateOfPublic: '15/09/2023',
+                      ),
+                      AnnouncementWidget(
+                        onTap: () {},
+                        announcementImage: ImageHelper.loadFromAsset(
+                            AssetHelper.announcementImage),
+                        title: 'International Band Museum312312321312',
+                        author: 'Nhan Nguyen',
+                        dateOfPublic: '15/09/2023',
+                      ),
+                      AnnouncementWidget(
+                        onTap: () {},
+                        announcementImage: ImageHelper.loadFromAsset(
+                            AssetHelper.announcementImage),
+                        title: 'International Band Museum312312321312',
+                        author: 'Nhan Nguyen',
+                        dateOfPublic: '15/09/2023',
+                      ),
                     ],
                   ),
                 ),
-              ),
-              const SizedBox(height: kMediumPadding / 2),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                child: Text('Announcement',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20)),
-              ),
-              const SizedBox(height: kMediumPadding / 2),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    AnnouncementWidget(
-                      onTap: () {},
-                      announcementImage: ImageHelper.loadFromAsset(
-                          AssetHelper.announcementImage),
-                      title: 'International Band Museum312312321312',
-                      author: 'Nhan Nguyen',
-                      dateOfPublic: '15/09/2023',
-                    ),
-                    AnnouncementWidget(
-                      onTap: () {},
-                      announcementImage: ImageHelper.loadFromAsset(
-                          AssetHelper.announcementImage),
-                      title: 'International Band Museum312312321312',
-                      author: 'Nhan Nguyen',
-                      dateOfPublic: '15/09/2023',
-                    ),
-                    AnnouncementWidget(
-                      onTap: () {},
-                      announcementImage: ImageHelper.loadFromAsset(
-                          AssetHelper.announcementImage),
-                      title: 'International Band Museum312312321312',
-                      author: 'Nhan Nguyen',
-                      dateOfPublic: '15/09/2023',
-                    ),
-                  ],
+                const SizedBox(height: kMediumPadding / 2),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  child: Text('New Product',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20)),
                 ),
-              ),
-              const SizedBox(height: kMediumPadding / 2),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                child: Text('New Product',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20)),
-              ),
-              const SizedBox(height: kMediumPadding / 2),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    AnnouncementWidget(
-                      onTap: () {},
-                      announcementImage: ImageHelper.loadFromAsset(
-                          AssetHelper.announcementImage),
-                      title: 'International Band Museum312312321312',
-                      author: 'Nhan Nguyen',
-                      dateOfPublic: '15/09/2023',
-                    ),
-                    AnnouncementWidget(
-                      onTap: () {},
-                      announcementImage: ImageHelper.loadFromAsset(
-                          AssetHelper.announcementImage),
-                      title: 'International Band Museum312312321312',
-                      author: 'Nhan Nguyen',
-                      dateOfPublic: '15/09/2023',
-                    ),
-                    AnnouncementWidget(
-                      onTap: () {},
-                      announcementImage: ImageHelper.loadFromAsset(
-                          AssetHelper.announcementImage),
-                      title: 'International Band Museum312312321312',
-                      author: 'Nhan Nguyen',
-                      dateOfPublic: '15/09/2023',
-                    ),
-                  ],
+                const SizedBox(height: kMediumPadding / 2),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      AnnouncementWidget(
+                        onTap: () {},
+                        announcementImage:
+                            ImageHelper.loadFromAsset(AssetHelper.productImage),
+                        title: 'International Band Museum312312321312',
+                        author: 'Nhan Nguyen',
+                        dateOfPublic: '15/09/2023',
+                      ),
+                      AnnouncementWidget(
+                        onTap: () {},
+                        announcementImage:
+                            ImageHelper.loadFromAsset(AssetHelper.productImage),
+                        title: 'International Band Museum312312321312',
+                        author: 'Nhan Nguyen',
+                        dateOfPublic: '15/09/2023',
+                      ),
+                      AnnouncementWidget(
+                        onTap: () {},
+                        announcementImage: Container(
+                            child: ImageHelper.loadFromAsset(
+                                AssetHelper.productImage)),
+                        title: 'International Band Museum312312321312',
+                        author: 'Nhan Nguyen',
+                        dateOfPublic: '15/09/2023',
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: kMediumPadding),
-            ],
+                const SizedBox(height: kMediumPadding),
+              ],
+            ),
           ),
         ),
       ),
